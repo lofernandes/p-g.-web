61158ee566406631ec88fe2acdad968d60de84c1
lofernandes <166837139+lofernandes@users.noreply.github.com>
2025-06-09 07:49:05 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'Qual o maior oceano do planeta?',
+    'Oceano Pacífico'
+)
+
+criaCartao(
+    'Literatura',
+    'Quem escreveu o livro "O alienista"?',
+    'Machado de Assis.'
+)
+
+criaCartao(
+    'História',
+    'A Guerra Fria aconteceu entre quais potências?',
+    'Estados Unidos e União Soviética'
+)
+
+criaCartao(
+    'Matemática',
+    'O que é a hipotenusa?',
+    'A soma dos quadrados dos catetos'
+)
